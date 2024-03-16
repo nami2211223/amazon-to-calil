13a6d6fa85bda2c11a2246cb5337109510a0904f
nami <148037725+nami2211223@users.noreply.github.com>
2024-03-16 19:00:27 +0900

    最初のコミット

---
+++ amazon-to-calil.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name     Amazon to Calil
+// @match    https://www.amazon.co.jp/*
+// ==/UserScript==
+
+{
+    main()
+
+    function main () {
+        if (shouldSetup()) {
+            setup()
+        }
+    }
+
+    function shouldSetup () {
+        return isBookPage()
+    }
+
+    function isBookPage () {
+        const {classList} = document.querySelector('#dp')
+        return (
+            classList.contains('book')
+            || classList.contains('book_mobile')
+        )
+    }
+
+    function setup () {
+        setupCSS()
+        setupButton()
+    }
+
+    function setupCSS () {
+        const html = `
+            <style>
+                .calil-container {
+                    position: relative;
+                }
+
+                .calil-link {
+                    z-index: 999999;
+                    position: absolute;
+                    top: 0;
+                    right: 0;
+                }
+            </style>
+        `
+        document.querySelector('head').insertAdjacentHTML('beforeend', html)
+    }
+
+    function setupButton () {
+        const asin = findAsin()
+        const html = `
+            <div class="calil-container">
+                <a class="calil-link" href="https://calil.jp/book/${asin}" target="_blank">
+                    <img src="https://calil.jp/favicon-32x32.png">
+                </a>
+            </div>
+        `
+        document.querySelector('#dp').insertAdjacentHTML('afterbegin', html)
+    }
+
+    function findAsin () {
+        return document.querySelector('link[rel=canonical]').href.split('/').pop()
+    }
+}
