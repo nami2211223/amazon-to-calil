20022bd2e9d77f5afc2dab58e1e9b07ad832526c
nami <148037725+nami2211223@users.noreply.github.com>
2024-03-16 19:06:10 +0900

    修正

--- amazon-to-calil.js
+++ amazon-to-calil.js
@@ -4,62 +4,62 @@
 // ==/UserScript==
 
 {
-    main()
+	main()
 
-    function main () {
-        if (shouldSetup()) {
-            setup()
-        }
-    }
+	function main () {
+		if (shouldSetup()) {
+			setup()
+		}
+	}
 
-    function shouldSetup () {
-        return isBookPage()
-    }
+	function shouldSetup () {
+		return isBookPage()
+	}
 
-    function isBookPage () {
-        const {classList} = document.querySelector('#dp')
-        return (
-            classList.contains('book')
-            || classList.contains('book_mobile')
-        )
-    }
+	function isBookPage () {
+		const {classList} = document.querySelector('#dp')
+		return (
+			classList.contains('book')
+			|| classList.contains('book_mobile')
+		)
+	}
 
-    function setup () {
-        setupCSS()
-        setupButton()
-    }
+	function setup () {
+		setupCSS()
+		setupButton()
+	}
 
-    function setupCSS () {
-        const html = `
-            <style>
-                .calil-container {
-                    position: relative;
-                }
+	function setupCSS () {
+		const html = `
+			<style>
+				.calil-container {
+					position: relative;
+				}
 
-                .calil-link {
-                    z-index: 999999;
-                    position: absolute;
-                    top: 0;
-                    right: 0;
-                }
-            </style>
-        `
-        document.querySelector('head').insertAdjacentHTML('beforeend', html)
-    }
+				.calil-link {
+					z-index: 999999;
+					position: absolute;
+					top: 0;
+					right: 0;
+				}
+			</style>
+		`
+		document.querySelector('head').insertAdjacentHTML('beforeend', html)
+	}
 
-    function setupButton () {
-        const asin = findAsin()
-        const html = `
-            <div class="calil-container">
-                <a class="calil-link" href="https://calil.jp/book/${asin}" target="_blank">
-                    <img src="https://calil.jp/favicon-32x32.png">
-                </a>
-            </div>
-        `
-        document.querySelector('#dp').insertAdjacentHTML('afterbegin', html)
-    }
+	function setupButton () {
+		const asin = findAsin()
+		const html = `
+			<div class="calil-container">
+				<a class="calil-link" href="https://calil.jp/book/${asin}" target="_blank">
+					<img src="https://calil.jp/favicon-32x32.png">
+				</a>
+			</div>
+		`
+		document.querySelector('#dp').insertAdjacentHTML('afterbegin', html)
+	}
 
-    function findAsin () {
-        return document.querySelector('link[rel=canonical]').href.split('/').pop()
-    }
+	function findAsin () {
+		return document.querySelector('link[rel=canonical]').href.split('/').pop()
+	}
 }
